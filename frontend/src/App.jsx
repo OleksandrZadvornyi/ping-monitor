5630ed9ba4326017e459c8b326362fb87eba6f75
Oleksandr Zadvornyi <zadvornyi.alex16@gmail.com>
2025-08-01 21:47:43 +0300

    style: format code with Prettier

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,30 @@
-import LatencyGraph from './components/LatencyGraph';
-import './index.css'; 
+import LatencyGraph from "./components/LatencyGraph";
+import "./index.css";
 
 function App() {
   return (
-    <div style={{
-      height: '100vh',
-      display: 'flex',
-      flexDirection: 'column',
-      padding: '1rem',
-      boxSizing: 'border-box',
-      overflow: 'hidden',
-    }}>
-      <div style={{ display: 'flex', alignItems: 'center', marginBottom: '1rem' }}>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        padding: "1rem",
+        boxSizing: "border-box",
+        overflow: "hidden",
+      }}
+    >
+      <div
+        style={{ display: "flex", alignItems: "center", marginBottom: "1rem" }}
+      >
         <img
           src="./icon-default.png"
           alt="Logo"
-          style={{ width: 48, height: 48, marginRight: '1rem', borderRadius: '20%' }}
+          style={{
+            width: 48,
+            height: 48,
+            marginRight: "1rem",
+            borderRadius: "20%",
+          }}
         />
         <h1 style={{ margin: 0 }}>Ping Monitor</h1>
       </div>
